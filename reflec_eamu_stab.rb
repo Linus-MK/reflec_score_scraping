93dd77383096728595fbc5fa74e4a7ca6c071e83
Linus-MK <murmur2winzcud37dle@gmail.com>
2017-03-28 20:43:50 +0900

    最初にURL文字列を抽出して、ページ遷移がうまくいくように改良

--- reflec_eamu_stab.rb
+++ reflec_eamu_stab.rb
@@ -30,34 +30,45 @@ sleep 3
 # driver.find_element(:class, "login_btn textindent").click →エラーになる
 password.submit
 
-# sleep 3
-
-# 方法1：できるだけSeleniumで処理する
 musics = driver.find_elements(:class, "music_jkimg")
 
 # p musics[0].find_element(:tag_name, "a").text # -> "Artifacter" 曲名が取得できる
 # p musics[0].find_element(:tag_name, "a").attribute("onclick") #-> "musicDlg.show('m_info.html?id=iaRaUMiHe3aoA1iSVTTG8DwFLVMJ%2FKI5Pihk7kODHC4%3D')"遷移先のURLが取得できる
 
-musics.each do |music|
-	onclick_str= music.find_element(:tag_name, "a").attribute("onclick")
-	title= music.find_element(:tag_name, "a").text
-	if /id=([\d\w%]+)'/ =~ onclick_str then
+#ページ遷移するとmusicsの参照先が変わるので、最初にURLを配列に入れる
+urls = musics.map{|music| music.find_element(:tag_name, "a").attribute("onclick") }
+
+urls.each do |url|
+	if /id=([\d\w%]+)'/ =~ url then
 		driver.navigate.to "http://p.eagate.573.jp/game/reflec/volzza/p/music/m_info.html?id=" + $1
+		
+		# 方法1：できるだけSeleniumで処理する
 		puts "music name : #{driver.find_element(:class, "music_dataname").text }"
 		puts "artist name : #{driver.find_element(:class, "music_artist").text }"
+		
+		versions = driver.find_elements(:class, "music_info_box1_cont music_data_box sp_boxpink_A")
+		# 現行バージョンとOLDの2つが手に入る
+		versions.each do |ver|
+			difs = ver.find_elements(:class, "music_info_box2")
+			#各難易度が手に入る
+		
+#		//*[@id="musicPage"]/div[2]
+		
 		puts "score is #{driver.find_element(:xpath, %!//*[@id="musicPage"]/div[2]/div[4]/div/dl/dd[3]! ).text }"
+		sleep 3
 	else
 		raise "ERROR -- invalid URL. music title: #{title}, onclick: #{onclick_str} "
 	end
 	
 	sleep 3
 end
-# 1曲だけ処理して画面が閉じてしまう……
 
-
-
-
-
+=begin
+最終的にはCSVに吐き出したい
+	いきなりCSVにprintf的なことをするか
+	配列にデータを保持してそれを最後にCSVにするか
+	拡張性を考えると、手元にデータを一旦落としたほうがその後の処理を新規追加しやすいので、後者で行こう
+=end
 
 
 
